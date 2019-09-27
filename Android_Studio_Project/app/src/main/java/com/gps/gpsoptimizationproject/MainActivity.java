5f87b58e702d193b59214c5f37310ec087a0d172
Guifindor <42903599+Guifindor@users.noreply.github.com>
2019-09-26 22:27:24 -0400

    Added some functionality for the button

--- Android_Studio_Project/app/src/main/java/com/gps/gpsoptimizationproject/MainActivity.java
+++ Android_Studio_Project/app/src/main/java/com/gps/gpsoptimizationproject/MainActivity.java
@@ -11,9 +11,13 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    static boolean textstatus = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,25 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        final TextView gpsIndicator = (TextView) findViewById(R.id.GPSindicator);
+
+        //Making the button do something
+        Button gpsButton = (Button) findViewById(R.id.GPSToggle);
+
+        gpsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(textstatus){
+                    textstatus = false;
+                    gpsIndicator.setText("Hello World again!");
+                }
+                else{
+                    textstatus = true;
+                    gpsIndicator.setText("Something");
+                }
+            }
+        });
+
     }
 
     @Override
